6e73e599f9599de1dae62f2848f6498fdb38ceec
jeremyb31 <jeremy53561@gmail.com>
2018-06-02 11:11:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make -C /lib/modules/$(uname -r)/build M=$(pwd) clean
 cp /usr/src/linux-headers-$(uname -r)/.config .
-cp /usr/src/linux-headers-$(uname -r)-generic/Module.symvers .
+cp /usr/src/linux-headers-$(uname -r)/Module.symvers .
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 sudo cp btusb.ko /lib/modules/$(uname -r)/kernel/drivers/bluetooth/btusb.ko
